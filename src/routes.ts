93c8e4b21c213c36578b842b662da289b0a6b682
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-06-08 15:11:06 -0300

    finished category backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,23 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
 // -- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// -- ROTAS CATEGORY --
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// -- ROTAS PRODUCT --
+
 
 export { router };
